008a866d329cf2d70fe1cf243415a51b09d61669
DarshanMurthy <darshants.darshan1@gmail.com>
2018-09-30 08:26:10 -0700

    base code

---
+++ src/main/java/com/startup/compiler/zip/Zip.java
@@ -0,0 +1,6 @@
+package com.startup.compiler.zip;
+
+public class Zip {
+    static final int ZIP64_ENDSIG = 0x06064b50;
+
+}
---
+++ src/test/java/com/startup/compiler/zip/ZipTest.java
@@ -0,0 +1,7 @@
+package com.startup.compiler.zip;
+
+import static org.junit.Assert.*;
+
+public class ZipTest {
+
+}
