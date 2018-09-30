fce408f6726a7a87305851b8ed611a63fd8481f9
DarshanMurthy <darshants.darshan1@gmail.com>
2018-09-30 10:00:17 -0700

    added zip parser

--- src/main/java/com/startup/compiler/zip/Zip.java
+++ src/main/java/com/startup/compiler/zip/Zip.java
@@ -1,6 +1,25 @@
 package com.startup.compiler.zip;
 
+import java.util.Iterator;
+import java.util.Map;
+
 public class Zip {
     static final int ZIP64_ENDSIG = 0x06064b50;
+    static final int LOCHDR = 30; //LOC header size
+
+    static class ZipIterator implements Iterator<Map.Entry>{
+
+
+
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+
+        @Override
+        public Map.Entry next() {
+            return null;
+        }
+    }
 
 }
--- src/test/java/com/startup/compiler/zip/ZipTest.java
+++ src/test/java/com/startup/compiler/zip/ZipTest.java
@@ -1,7 +1,24 @@
 package com.startup.compiler.zip;
 
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.zip.ZipOutputStream;
+
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
 import static org.junit.Assert.*;
 
+@RunWith(JUnit4.class)
 public class ZipTest {
+    @Rule public  final  TemporaryFolder temporaryFolder
+            = new TemporaryFolder();
+
+
 
 }
